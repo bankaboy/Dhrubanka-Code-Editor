9ae4d54754cc9399e6a017d0b590f14ff1e323b6
Dhrubanka Dutta <dhrubanka.99dutta@gmail.com>
2026-04-13 20:52:21 +0100

    created dummy component to display response from dummy backend

---
+++ frontend/src/components/dummy_frontend/callResponse/CallResponse.tsx
@@ -0,0 +1,27 @@
+
+
+interface CallResponseProps {
+  methodType: string;
+  endpoint: string;
+  response: string;
+}
+
+function Heading({
+  methodType = "GET",
+  endpoint = "/api/data",
+  response = "Default response if nothing received from backend"
+}: CallResponseProps) {
+  return (
+    <>
+      <h2 className="text-4xl font-bold leading-tight text-black sm:text-4xl mb-0">
+        {methodType}
+      </h2>
+      <hr className="h-px w-full bg-black border-none mt-0" />
+      <p>
+        The {methodType} call was made to the C++ REST API hosted on "http://localhost:8080/api/data". The service responded with the response: {response}
+      </p>
+    </>
+  );
+}
+
+export default Heading;
