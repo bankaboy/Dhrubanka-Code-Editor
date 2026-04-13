19aa16059b1edcf24f0931b5ca3dfc8ba77a0deb
Dhrubanka Dutta <dhrubanka.99dutta@gmail.com>
2026-04-13 17:12:13 +0100

    added heading component for dummy frontend

---
+++ frontend/src/components/dummy_frontend/heading/Heading.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { DefaultInputs } from "@/helpers/constants";
+
+interface HeadingProps {
+  headingText: string;
+}
+
+function Heading({
+  headingText = DefaultInputs.DEFAULT_HEADING_TEXT,
+}: HeadingProps) {
+  return (
+    <>
+      <h2 className="text-4xl font-bold leading-tight text-black sm:text-4xl mb-0">
+        {headingText}
+      </h2>
+      <hr className="h-px w-full bg-black border-none mt-0" />
+    </>
+  );
+}
+
+export default Heading;
