452e4a07b8c9b26e2ea4393a23e9064c6d9551d9
Dhrubanka Dutta <dhrubanka.99dutta@gmail.com>
2026-05-03 23:08:10 +0100

    updated babel config for ESM Config

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,6 +1,6 @@
-module.exports = {
+export default {
   presets: [
     '@babel/preset-env',
-    ['@babel/preset-react', {runtime: 'automatic'}],
+    ['@babel/preset-react', { runtime: 'automatic' }],
   ],
 };
