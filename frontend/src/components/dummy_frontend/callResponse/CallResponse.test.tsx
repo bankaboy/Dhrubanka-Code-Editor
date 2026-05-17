1b5641c89dcbb99462477b4ba9cad8945780ea85
Dhrubanka Dutta <dhrubanka.99dutta@gmail.com>
2026-05-17 20:21:04 +0100

    added test to check CallResponse shows methodType displays as h2

--- frontend/src/components/dummy_frontend/callResponse/CallResponse.test.tsx
+++ frontend/src/components/dummy_frontend/callResponse/CallResponse.test.tsx
@@ -7,4 +7,10 @@ describe('CallResponse', () => {
         render(<CallResponse methodType='POST'/>) // default is GET, so using POST
         expect(screen.getByText('POST')).toBeInTheDocument();
     })
+
+    it('renders the provided method type text as h2', () => {
+        render(<CallResponse methodType='POST'/>) // default is GET, so using POST
+        const heading = screen.getByRole('heading', { level: 2 });
+        expect(heading).toHaveTextContent('POST');
+    })
 })
